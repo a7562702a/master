2812b2adf3b35458a7045c727d840f3120d7a740
kmin9 <kmin9@LAPTOP-CRCTIDMV>
2020-04-17 16:37:33 +0900

    IMP 제거

--- src/main/java/net/shopping/hello/guestDAO.java
+++ src/main/java/net/shopping/hello/guestDAO.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public class guestDAO implements HelloDAOImp { 
+public class guestDAO { 
    
 	@Autowired
 	SqlSessionTemplate temp ;
--- src/main/java/net/shopping/hello/guestDAOImp.java
+++
@@ -1,8 +0,0 @@
-package net.shopping.hello;
-
-import java.util.List;
-
-public interface guestDAOImp {
-  
-  public void dbInsert(HelloDTO dto);
-}//interface end

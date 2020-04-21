8addbc9e340938cbbb8ccf5d0b95513eaa69a4d7
cyamor <cyamor@naver.com>
2020-04-21 10:32:47 +0900

    4/21 AM 10:32 Merged

---
+++ src/main/java/shopping/crud/HomeController.java
@@ -0,0 +1,46 @@
+package shopping.crud;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HomeController {   
+   private static final Logger logger=LoggerFactory.getLogger(BoardController.class);
+   
+   
+   
+   //@Autowired
+   //private ServletContext  application;
+   
+   
+   @RequestMapping(value="/home.do")
+   public String goHome(Model model) {
+      return "home";
+   }//end
+
+
+}//BoardController class END
--- src/main/webapp/indexHello.jsp
+++ src/main/webapp/indexHello.jsp
@@ -5,23 +5,14 @@
 <meta charset="UTF-8">
 <title></title>
 	<script type="text/javascript">
-		
+		setTimeout("location.href='home.do'",2000);
 	</script>
 </head>
 <body>
 
 	<div align="center">
 		<font size=6 color=blue><b>Spring.....Waiting...Loading...</b></font> <p>
-	    <img src="resources/images/ddd.gif">
-	    <video src="resources/images/scuba.mp4" 
-	           autoplay="autoplay"  
-	           muted="muted" 
-	           loop="loop" 
-	           width=750 
-	           height=350  
-	           controls="controls"> 
-	     </video>	
-	    <img src="resources/images/ddd.gif">
+	  
 	</div>
 </body>
 </html>

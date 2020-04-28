2688f5a20cd3b8aaa86c2154ca9668fa14035698
cyamor <cyamor@naver.com>
2020-04-28 10:56:32 +0900

    2

--- src/main/java/shopping/crud/product/ProductReviewController.java
+++ src/main/java/shopping/crud/product/ProductReviewController.java
@@ -1,6 +1,10 @@
 package shopping.crud.product;
 
+import java.io.File;
 import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,18 +23,81 @@ public class ProductReviewController {
 	@Autowired
 	ProductReviewDAO pdao;
 	
+	@Autowired
+	private ServletContext application;
+	
 	private static final Logger logger = LoggerFactory.getLogger(ProductReviewController.class);
 	
 	
 	
 	@RequestMapping(value="/productReviewList.do")
-	public String product_select(Model model) {
-		List<ProductQuestDTO> PD = pdao.dbSelect();
-		model.addAttribute("PD",PD);	
-		return "ProductReviewList";
+	public String productR_select(HttpServletRequest request, Model model) {
+		 String pnum;
+		 int pageNUM, pagecount;
+		 int start, end; 
+		 int startpage, endpage;  
+		 int temp;	
+		 String sqry="";
+		 String skey="", sval="";
+		 String returnpage=""; 
+		 int Rnumber; 
+		 
+		 pnum=request.getParameter("pagePRNum");
+		 if(pnum=="" || pnum==null) {
+			 pnum="1";
+		 }
+		 
+		 pageNUM=Integer.parseInt(pnum);
+		 int Gtotal=pdao.dbCount();
+		 start=(pageNUM-1)*10+1;
+		 end=pageNUM*10;
+	 
+		 if(Gtotal%10==0) {pagecount = Gtotal/10;}
+		 else {pagecount = (Gtotal/10)+1;}
+		 
+		 temp=(pageNUM-1)%10;
+		 startpage=pageNUM-temp;
+		 endpage=startpage+9;
+		 if(endpage>pagecount) {endpage=pagecount;}
+	
+		
+		
+		List<ProductReviewDTO> PD = pdao.dbSelect(start,end);
+		 model.addAttribute("pageNUM", pageNUM);
+		 model.addAttribute("startpage", startpage);
+		 model.addAttribute("endpage", endpage);
+		 model.addAttribute("pagecount", pagecount);
+		
+		 
+		 model.addAttribute("PD",PD);
+		 return "ProductReviewList";
 	}//end
 	
+	@RequestMapping(value="/productReview.do")
+	public String productR_write() {	
+		return "ProductReviewWrite";
+	}//end
+	
+	@RequestMapping("/productReviewInsert.do")
+	public String productR_insert(ProductReviewDTO dto,HttpServletRequest request) {
+		String path=application.getRealPath("/resources/upload");
+		String img=dto.getUpload_f().getOriginalFilename();
+		File file = new File(path, img); 
+		try {
+			dto.getUpload_f().transferTo(file);
+		}catch (Exception e) {System.out.println(e);	}		
+		dto.setFile1(img);
+		pdao.dbInsert(dto);
+		return "redirect:/productReviewList.do";
+	}//end
 
+	@RequestMapping("/productReviewDelete.do")
+	public String quest_insert(@RequestParam("rid") int questnum) {
+		pdao.dbDelete(questnum);
+		return "redirect:/productReviewList.do";
+	}//end
+	
 }//BoardReplyController class END
+
 
 
